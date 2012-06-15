8158fd3566e2c63cf3a78db28db87a5e99c82ec2
Michal Rudolf <michal@rudolf.waw.pl>
2012-06-15 16:55:14 +0200

    Configure fixes.

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#include "autoconfig.h"
+
+/* Ubuntu needs it */
+
+#define HAVE_STDINT_H 1
+#define HAVE_MBSTATE_T 1
+#define HAVE_NAMESPACES 1
+#undef NOT_USING_DJVU_NAMESPACE
+#define AUTOCONF 1
+#define HAVE_STDINCLUDES 1
+#define THREADMODEL POSIXTHREADS
+#define CPP_SUPPORTS_EXCEPTIONS 1
